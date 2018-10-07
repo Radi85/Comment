d9a7801fcfb10f7a2e836c0f8f6700fab5df770e
Radi85 <mus.radi85@gmail.com>
2018-10-07 16:37:04 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1.2',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
