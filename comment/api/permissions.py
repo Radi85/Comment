48f1e48e3a350e938a4bc14e4f5e3fbeda751337
Radi85 <mus.radi85@gmail.com>
2019-01-14 18:15:56 +0100

    handle more exceptions

--- comment/api/permissions.py
+++ comment/api/permissions.py
@@ -1,5 +1,4 @@
 from django.contrib.contenttypes.models import ContentType
-from django.db.models import Q
 from rest_framework import permissions
 
 
@@ -11,8 +10,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # GET, HEAD or OPTIONS requests are SAFE_METHODS.
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # PUT and DELETE permissions are only allowed to the author of the blog.
+        # PUT and DELETE permissions are allowed to the owner of the comment.
         return obj.user == request.user
 
 
@@ -21,15 +19,21 @@ class QuerySetPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         model_type = request.GET.get("type")
-        slug = request.GET.get("slug", None)
-        pk = request.GET.get("id", None)
+        pk = request.GET.get("id")
         try:
-            model = ContentType.objects.get(model=model_type.lower()).model_class()
-            model_obj = model.objects.filter(Q(id=pk)|Q(slug=slug))
-            if not model_obj.exists() and model_obj.count() != 1:
-                self.message = "this is not a valid id or slug for this model"
+            model_type = model_type.lower()
+            ct = ContentType.objects.get(model=model_type).model_class()
+            Model = ct.objects.filter(id=pk)
+            if not Model.exists() and Model.count() != 1:
+                self.message = "this is not a valid id for this model"
                 return False
             return True
         except ContentType.DoesNotExist:
             self.message = "this is not a valid content type."
             return False
+        except AttributeError:
+            msg = "content type must be specified to retrieve comment list"
+            self.message = msg
+            return False
+        except ValueError:
+            self.message = "the id must be an integer"
