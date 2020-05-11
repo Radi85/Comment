08b1ee492f006c4905d6a9300428202411cff46a
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2020-05-12 04:27:02 +0530

    Cleanup Reaction model, add function for views
    Changes made to Reaction model
    - Reaction choices are now made from IntEnum, since models.TextChoices is only supported in Django 3.0
    - related_name for foreign keys made plural
    - move logic for set_reaction to ReactionInstance model
    
    Changes to ReactionInstance
    - add function to override save function.
    - add a function to be executed on pre-delete signal.
    - set_reaction method is now a classmethod.
    - clean up set_reaction and refactor it.
    
    Changes to Comment model
    - add property for showing like and dislike count.
    
    Add urlpattern for accepting requests for reaction.
    
    Changes to views
    - views is now a package, move earlier code to comments module inside it
    - add module for reactions
    
    Changes to templates
    - show like and dislike count alongside the comment content

--- comment/api/urls.py
+++ comment/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from comment.api import views
+from comment.views import react
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view(), name='comments-list'),
     path('comments/create/', views.CommentCreate.as_view(), name='comments-create'),
     path('comments/<int:pk>/', views.CommentDetail.as_view(), name='comment-detail'),
+    path('comments/react/<int:comment_id>/<str:reaction>', react, name='react')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
