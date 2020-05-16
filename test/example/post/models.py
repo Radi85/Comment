6c1a7adda9c6437b82b991161d88da1c49532383
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2020-05-16 16:45:15 +0530

    Change profile app name to resolve name conflics with ipython module
    - make other corresponding changes
    - fixes #11
    
    Change some imports order to follow PEP8 conventions

--- test/example/post/models.py
+++ test/example/post/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericRelation
+
 from comment.models import Comment
 
 
--- test/example/user_profile/tokens.py
+++ test/example/user_profile/tokens.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
+
+
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
