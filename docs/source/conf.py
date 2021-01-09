d2247a68dd624cc36a87f89cc2ea627bb6b44590
Radi85 <mus.radi85@gmail.com>
2021-01-09 15:46:32 +0100

    feat(#142): extend ui customization
    closes #142

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -112,7 +112,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Django-comment-dabdoc'
+htmlhelp_basename = 'Django-comments-dabdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
